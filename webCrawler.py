9c4f371a1388d94ffc278583d2ff8214b67c367e
Alan Stock <astock517@gmail.com>
2017-09-26 22:58:44 -0500

    Add additional function requirements.

--- webCrawler.py
+++ webCrawler.py
@@ -10,8 +10,10 @@ import requests
 
 def main():
 
+    url = "http://www.nba.com/cavaliers/"
+
     q = Queue()
-    q.put("http://www.nba.com/cavaliers/")
+    q.put(url)
     relevant_urls = set()
     scraped_urls = set()
 
@@ -28,16 +30,9 @@ def main():
                 q.put(link_str)
                 scraped_urls.add(link_str)
 
-            if 'Lebron' in link_str or 'lebron' in link_str:
-                if link_str.startswith('/url?q='):
-                    link_str = link_str[7:]
-                if '&' in link_str:
-                    i = link_str.find('&')
-                    link_str = link_str[:i]
-                if link_str.startswith('http') and 'google' not in link_str:
-                    if link_str not in relevant_urls:
-                        relevant_urls.add(link_str)
-                        scrape(link_str)
+                if 'Lebron' in link_str or 'lebron' in link_str and link_str not in relevant_urls:
+                    relevant_urls.add(link_str)
+                    scrape(link_str)
 
     with open("urls.txt", "w") as output:
         output.write(str(relevant_urls))
@@ -54,6 +49,21 @@ def scrape(url):
     with open("{}.txt".format(url.replace("/","-")[-25:]), "w") as output:
         output.write(str(text))
 
+
+'''
+Write a function to clean up the text. You might need to delete newlines and tabs. 
+Extract sentences with NLTK’s sentence tokenizer. Write the sentences for each file to a new file. 
+That is, if you have 15 files in, you have 15 files out. 
+You might need to clean up the cleaned up files manually to delete irrelevant material. 
+'''
+
+
+'''
+Write a function to extract at least 10 important terms from the pages using an importance measure 
+such as term frequency. First, it’s a good idea to lower-case everything, remove stopwords and punctuation. 
+Then build a vocabulary of unique terms. Create a dictionary of unique terms where the key is the token and 
+the value is the count across all documents.  Print the top 25-40 terms.
+'''
 
 if __name__ == "__main__":
     main()
