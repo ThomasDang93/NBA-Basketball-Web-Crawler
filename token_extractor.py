4d33af7a3c71ede09575b51effecfc2f092b86b8
ThomasDang93 <thomasdang93@gmail.com>
2017-09-27 22:46:59 -0500

    Update token_extractor.py
    removed unwanted set() functions

--- token_extractor.py
+++ token_extractor.py
@@ -5,21 +5,26 @@ from nltk.corpus import stopwords
 TOP_TERMS_LIMIT = 6
 def main():
     vocab_sum = Counter({})
-    fdist_sum = nltk.FreqDist()
+    #fdist_sum = Counter(nltk.FreqDist())
 
     text = "i hate you hate why are mistakes! you hate my lebron mistakes wade is to me my carry lover of dine"
     text2nd = "can you see my mistakes he so i can lebron need you to be a there very careful. lebron? dine"
 
-    fdist, vocab = extract_tokens(text)
-    fdist_sum += fdist
+    vocab = extract_tokens(text)
+    #fdist_sum += Counter(fdist)
     vocab_sum += Counter(vocab)
 
-    fdist, vocab = extract_tokens(text2nd)
-    fdist_sum += fdist
+    vocab = extract_tokens(text2nd)
+    #fdist_sum += Counter(fdist)
     vocab_sum += Counter(vocab)
 
 
-    print(fdist_sum)
+    #print(fdist_sum)
+    #print('\n')
+    print("Merged dictionary using Counter:")
+    print(vocab_sum)
+    print('\n')
+    print("Top 6 terms from the merged dictionary:")
     print(vocab_sum.most_common(TOP_TERMS_LIMIT))
 
 
@@ -29,9 +34,8 @@ def extract_tokens(text):
 
     stop_words = set(stopwords.words('english'))
     tokens = nltk.word_tokenize(text.lower())
-    tokens = [t for t in set(tokens) if t not in stop_words and t not in string.punctuation]
-    unique_tokens = set(tokens)
-    fdist = nltk.FreqDist(unique_tokens)
+    tokens = [t for t in tokens if t not in stop_words and t not in string.punctuation]
+    #fdist = nltk.FreqDist(tokens)
 
     vocab = {}  # dictionary
     for token in tokens:
@@ -39,17 +43,15 @@ def extract_tokens(text):
             vocab[token] += 1
         else:
             vocab[token] = 1
-
+    print("Vocabulary of text without stopwords and punctuation:")
     print(vocab)
-    print(unique_tokens)
     print('\n')
+    #f = fdist.most_common(TOP_TERMS_LIMIT)
 
-    return fdist, vocab
+    return vocab
 
 if __name__ == "__main__":
     main()
-
-
 
 
 
