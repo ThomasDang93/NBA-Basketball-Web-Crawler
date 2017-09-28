1bdcf79d3041293538e4dbd30fc6ccb5187f9eff
ThomasDang93 <thomasdang93@gmail.com>
2017-09-27 21:15:54 -0500

    updated token_extractor.py
    Added functionality to sum up multiple dictionaries where each dictionary corresponds to a clean text file. This function can now get top 40 terms from that cumulative dictionary.
    
    Also, added small code to remove all punctuation, as they are needed according to the project description. This function is mainly used for debugging purposes if you want to play with it. The official function will be implemented in the webCrawler.py

--- token_extractor.py
+++ token_extractor.py
@@ -1,30 +1,55 @@
+from collections import Counter
 import nltk
-#nltk.download()
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-from nltk.book import*
 import string
+from nltk.corpus import stopwords
+TOP_TERMS_LIMIT = 6
+def main():
+    vocab_sum = Counter({})
+    fdist_sum = nltk.FreqDist()
 
-def token_extract(text):
-    tokens = word_tokenize(text)  # tokenize text
-    unique_tokens = set(tokens)     # set unique tokens
-    fdist = FreqDist(unique_tokens)  # get frequency distribution
+    text = "i hate you hate why are mistakes! you hate my lebron mistakes wade is to me my carry lover of dine"
+    text2nd = "can you see my mistakes he so i can lebron need you to be a there very careful. lebron? dine"
 
-    vocab = {}  #dictionary
-    for token in unique_tokens:
+    fdist, vocab = extract_tokens(text)
+    fdist_sum += fdist
+    vocab_sum += Counter(vocab)
+
+    fdist, vocab = extract_tokens(text2nd)
+    fdist_sum += fdist
+    vocab_sum += Counter(vocab)
+
+
+    print(fdist_sum)
+    print(vocab_sum.most_common(TOP_TERMS_LIMIT))
+
+
+
+
+def extract_tokens(text):
+
+    stop_words = set(stopwords.words('english'))
+    tokens = nltk.word_tokenize(text.lower())
+    tokens = [t for t in set(tokens) if t not in stop_words and t not in string.punctuation]
+    unique_tokens = set(tokens)
+    fdist = nltk.FreqDist(unique_tokens)
+
+    vocab = {}  # dictionary
+    for token in tokens:
         if token in vocab:
             vocab[token] += 1
         else:
             vocab[token] = 1
+
     print(vocab)
-    print(fdist)
+    print(unique_tokens)
+    print('\n')
+
     return fdist, vocab
 
-text = "i hate you hate"
-token_extract(text)
+if __name__ == "__main__":
+    main()
 
-'''
-Need to add functionality to print top 25-40 terms.
-'''
+
+
 
 
